cee0c730e86745165135d04f64bc4c47619f3b54
Alexandre Daoud <alex.daoud@mac.com>
2013-12-12 19:39:56 +0000

    work on index page

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -16,7 +16,7 @@ class ProjectsController < ApplicationController
         else
           @title = t("site.title")
           if current_user && current_user.recommended_projects.present?
-            @recommends = current_user.recommended_projects.limit(3)
+            @recommends = current_user.recommended_projects.limit(8)
           else
             @recommends = ProjectsForHome.recommends
           end
