c638c9855727eb56d3cb810e6c1a8916634b1962
Alexandre Daoud <alex.daoud@mac.com>
2013-12-22 15:15:39 +0000

    mandrill

--- config/initializers/sendgrid.rb
+++ config/initializers/sendgrid.rb
@@ -2,11 +2,13 @@ begin
   if Rails.env.production?
     ActionMailer::Base.smtp_settings = {
     address: 'smtp.mandrillapp.com',
-    port: '587',
+    port: '25',
+    enable_starttls_auto: true,
     authentication: :plain,
     user_name: Configuration[:mandrill_username],
     password: Configuration[:mandrill_password],
-    domain: 'mandrillapp.com'
+    authentication: 'login',
+    domain: 'tribaltears.com'
     }
     ActionMailer::Base.delivery_method = :smtp
   end
